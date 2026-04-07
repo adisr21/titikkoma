485e14ede92a7336da23053024e81c3859a3d832
Adi Sukarno Rachman <adi@jodapp.com>
2026-04-07 12:48:50 +0700

    fix: setup bash

--- deploy/setup.bash
+++ deploy/setup.bash
@@ -5,11 +5,12 @@ source "$(dirname "$0")/config.bash"
 mkdir -p "${HOME}/builds"
 mkdir -p "${HOME}/builds/build_initial"
 
-# Ensure correct ownership
 chown -R ubuntu:ubuntu "${BUILDS_DIR}"
 
-# Build the app for the first time
-BUILD_PATH="${HOME}"/builds/build_initial npm run build --prefix "${PROJECT_DIR}"
+npm install --prefix "${PROJECT_DIR}"
 
-# Create the initial "current" symlink
-cd "${BUILDS_DIR}" && ln -s "build_initial" "current"
+export PATH=$PATH:${PROJECT_DIR}/node_modules/.bin
+
+BUILD_PATH="${HOME}/builds/build_initial" npm run build --prefix "${PROJECT_DIR}"
+
+cd "${BUILDS_DIR}" && ln -sfn "build_initial" "current"
